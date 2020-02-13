3b1870cfd5c6a1ee1246cdf7f696fd0ae3ca73dd
Felipe <felipe.salazar.di@gmail.com>
2020-02-13 17:56:46 -0300

    kata calculadora, primer branch

---
+++ Kata_Kalkuladora/src/main/java/cl/desafiolatamprueba/Kata_Kalkuladora/App.java
@@ -0,0 +1,13 @@
+package cl.desafiolatamprueba.Kata_Kalkuladora;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )	{
+    	
+    	
+    }
+}
---
+++ Kata_Kalkuladora/src/main/java/cl/desafiolatamprueba/Kata_Kalkuladora/Kalkulator.java
@@ -0,0 +1,7 @@
+package cl.desafiolatamprueba.Kata_Kalkuladora;
+
+public class Kalkulator {
+
+	
+	
+}
